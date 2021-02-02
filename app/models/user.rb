e046f10cef0f87435913b20c53a78e538aa140b8
ayush893 <mauryaayush893@gmail.com>
2021-02-02 08:20:27 +0530

    adding oauth

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :validatable
+
+
+     def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(name: data["name"],
+           email: data["email"],
+           password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+end
+
+end
